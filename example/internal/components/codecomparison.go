f5ad042176101d0b549d6193c5ac22310bfb8c58
M. Price <zaptross@outlook.com>
2023-05-18 20:22:01 +1000

    feat(example): first working javascript example

--- example/internal/components/codecomparison.go
+++ example/internal/components/codecomparison.go
@@ -1,8 +1,25 @@
 package components
 
-import g "github.com/zaptross/gorgeous"
+import (
+	"fmt"
 
-func CodeComparison() *g.HTMLElement {
+	g "github.com/zaptross/gorgeous"
+	p "github.com/zaptross/gorgeous/example/internal/provider"
+	s "github.com/zaptross/gorgeous/example/internal/services"
+)
+
+func CodeComparison(titleElementId string) *g.HTMLElement {
+	theme := p.ThemeProvider.GetTheme()
+
+	es := s.GetElementService()
+
+	g.Class(".code-comparison-hover", g.CSSProps{
+		"background-color":   theme.Base02,
+		"box-shadow":         fmt.Sprintf("inset 0 0 0 2px %s", theme.Green),
+		"-webkit-box-shadow": fmt.Sprintf("inset 0 0 0 2px %s", theme.Green),
+		"-moz-box-shadow":    fmt.Sprintf("inset 0 0 0 2px %s", theme.Green),
+	})
+
 	return g.Div(g.EB{
 		ClassList: []string{"code-comparison"},
 		Children: g.CE{
@@ -23,5 +40,18 @@ func CodeComparison() *g.HTMLElement {
 			"justify-content": "space-between",
 			"text-align":      "left",
 		},
+		Script: g.JavaScript(g.JavaScript(getMouseEventScript(es, titleElementId))),
 	})
+}
+
+func getMouseEventScript(es string, titleId string) string {
+	titleElement := fmt.Sprintf(`%s('%s')`, es, titleId)
+	return fmt.Sprintf(`
+		thisElement.addEventListener('mouseover', () => {
+			%s?.classList.add('code-comparison-hover');
+		});
+		thisElement.addEventListener('mouseout', () => {
+			%s?.classList.remove('code-comparison-hover');
+		});
+	`, titleElement, titleElement)
 }
