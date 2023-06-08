1097cd9a0a985d399a3e4ed066b31943db97caaa
M. Price <zaptross@outlook.com>
2023-06-09 00:39:19 +1000

    feat(example): update example
    Adds examples of tree shaking, blending, and refactors old Class() calls

--- example/cmd/style.go
+++ example/cmd/style.go
@@ -8,50 +8,79 @@ import (
 func registerClasses() {
 	theme := prv.ThemeProvider.GetTheme()
 
-	g.Class("html, body", g.CSSProps{
-		"background-color": theme.Base03,
-		"height":           "100%",
-		"display":          "flex",
-		"flex-direction":   "column",
-		"align-items":      "center",
+	// Because this class is not used anywhere, it will be tree-shaken from the final css.
+	g.Class(&g.CSSClass{
+		Selector: ".to-be-tree-shaken",
+		Props: g.CSSProps{
+			"color": theme.Red,
+		},
 	})
 
-	g.Class("body", g.CSSProps{
-		"width": "60%",
+	g.Class(&g.CSSClass{
+		Selector: "html, body",
+		Include:  true,
+		Props: g.CSSProps{
+			"background-color": theme.Base03,
+			"height":           "100%",
+			"display":          "flex",
+			"flex-direction":   "column",
+			"align-items":      "center",
+		},
 	})
 
-	g.Class(".codeblock", g.CSSProps{
-		"background-color": theme.Base02 + " !important",
-		"height":           "100%",
-		"border":           "1px solid " + theme.Green,
-		"padding":          "0.5rem",
-		"font-family":      "monospace !important",
+	g.Class(&g.CSSClass{
+		Selector: "body",
+		Props: g.CSSProps{
+			"width": "60%",
+		},
 	})
 
-	g.Class("pre", g.CSSProps{
-		"background":  "none !important",
-		"text-shadow": "none !important",
+	// Customise the codeblock styling
+	g.Class(&g.CSSClass{
+		Selector: "pre",
+		Include:  true,
+		Props: g.CSSProps{
+			"background":  "none !important",
+			"text-shadow": "none !important",
+		},
 	})
-	g.Class("code", g.CSSProps{
-		"color":       theme.Base1 + " !important",
-		"background":  "none !important",
-		"text-shadow": "none !important",
+	g.Class(&g.CSSClass{
+		Selector: "code",
+		Include:  true,
+		Props: g.CSSProps{
+			"color":       theme.Base1 + " !important",
+			"background":  "none !important",
+			"text-shadow": "none !important",
+		},
 	})
-	g.Class(".operator", g.CSSProps{
-		"background": "none !important",
+	g.Class(&g.CSSClass{
+		Selector: ".operator",
+		Include:  true,
+		Props: g.CSSProps{
+			"background": "none !important",
+		},
 	})
 
-	g.Class("a", g.CSSProps{
-		"color":           theme.Cyan,
-		"text-decoration": "none",
+	g.Class(&g.CSSClass{
+		Selector: "a",
+		Props: g.CSSProps{
+			"color":           theme.Cyan,
+			"text-decoration": "none",
+		},
 	})
 
-	g.Class("a:hover", g.CSSProps{
-		"color":           theme.Violet,
-		"text-decoration": "none",
+	g.Class(&g.CSSClass{
+		Selector: "a:hover",
+		Props: g.CSSProps{
+			"color":           theme.Violet,
+			"text-decoration": "none",
+		},
 	})
 
-	g.Class(".code-comparison > div:first-child", g.CSSProps{
-		"margin-right": "1em",
+	g.Class(&g.CSSClass{
+		Selector: ".code-comparison > div:first-child",
+		Props: g.CSSProps{
+			"margin-right": "1em",
+		},
 	})
 }
